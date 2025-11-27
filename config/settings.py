2d376e42f06e01715e8c15b8721b00a72e1c575c
yves45839 <ouiyayves@gmail.com>
2025-11-27 11:58:38 +0000

    produit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e0m0an3&@t*boxg)ypht0*2@^6&zs42ia9b1i^=93=f3^_siaq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["samr.pythonanywhere.com"] 
+ALLOWED_HOSTS = ["samr.pythonanywhere.com", "127.0.0.1", "localhost"] 
 
 # Application definition
 
